ebd768ac4599dc3f826cbc8984558e5bea957ed8
jnelson <jnelson11@gmail.com>
2014-03-09 12:13:27 -0400

    Adding the tracking script

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,27 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var myApp = angular.module('myApp.controllers', []);
 
-  }])
-  .controller('MyCtrl2', [function() {
+var loginController = function($scope, $timeout) {
+  $scope.submitted = false;
 
-  }]);
+  $scope.closeAlert = function closeAlert() {
+    $scope.submitted = false;
+  }
+
+  $scope.login = function login(user) {
+    $scope.submitted = true;
+    user.timestamp = Date.now();
+    $scope.tracking.img = "/img/tracker.gif?" + $.param(user);
+    $timeout(function(){
+      $scope.closeAlert();
+    }, 3000);
+  }
+
+  $scope.tracking = {img:""};
+}
+
+myApp.controller('loginCtrl', loginController);
+myApp.controller('MyCtrl1', [function(){}]);
+myApp.controller('MyCtrl2', [function(){}]);
